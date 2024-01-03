e7c9a9adcd9ab167d7b2a4ddbf61a5631dc91ecb
sgsdgsge4 <155554163+sgsdgsge4@users.noreply.github.com>
2024-01-03 15:06:26 +0100

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,8 @@
+ msg = "</3";
+msg = "+PornHub+" + msg;position = 0;
+function scrolltitle() {
+document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
+if (position > msg.length) position = 0
+window.setTimeout("scrolltitle()",170);
+}
+scrolltitle();
